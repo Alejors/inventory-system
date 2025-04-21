a72a99ea7fe4f221f57d2ed1f7bda2016481d1c8
Alejandro Atria <alejandro.atria@enviame.io>
2025-04-21 17:05:08 -0500

    user DTO

--- api/src/dtos/userDTO.js
+++ api/src/dtos/userDTO.js
@@ -1,15 +1,16 @@
 class UserDTO {
-  constructor(username, email, password, role) {
+  constructor(username, email, password, role, companyId) {
     this.username = username;
     this.email = email;
     this.password = password;
     this.role = role;
+    this.companyId = companyId;
   }
 
   static fromObject(obj) {
-    return new UserDTO(obj.username, obj.email, obj.password, obj.role);
+    return new UserDTO(obj.username, obj.email, obj.password, obj.role, obj.companyId);
   }
-  
+
 }
 
 module.exports = UserDTO;
